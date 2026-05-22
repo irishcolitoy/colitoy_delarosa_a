f9306511363c215151bea1338d856ec6ddee9175
colitoy_delarosa_m <colitoyirish263@gamail.com>
2026-05-22 22:41:44 +0800

    Added controller, service, and model folders

---
+++ src/test/java/com/ws101/colitoy_delarosa_a/EcommerceApi/EcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws101.colitoy_delarosa_a.EcommerceApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
