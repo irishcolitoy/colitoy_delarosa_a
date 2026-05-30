0605e327b64ceb90b33041efb75d72fb9acf6ad5
colitoy_delarosa_a <colitoyirish263@gamail.com>
2026-05-30 14:30:25 +0800

    feat: complete product model fields

--- src/main/java/com/ws101/colitoy_delarosa_a/EcommerceApi/model/Product.java
+++ src/main/java/com/ws101/colitoy_delarosa_a/EcommerceApi/model/Product.java
@@ -13,14 +13,23 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String description;
+    private String category;
     private double price;
     private int stock;
 
     public Product() {
     }
 
-    public Product(String name, double price, int stock) {
+    public Product(
+            String name,
+            String description,
+            String category,
+            double price,
+            int stock) {
         this.name = name;
+        this.description = description;
+        this.category = category;
         this.price = price;
         this.stock = stock;
     }
@@ -39,6 +48,22 @@ public class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     public double getPrice() {
